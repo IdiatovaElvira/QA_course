516f84ea1198dc2d48da9e9948fea51514d75cca
idiyatovaelvira <idiatovaelvira@gmail.com>
2025-02-10 21:00:17 +0500

    add changes desidn

--- sql_task.sql
+++ sql_task.sql
@@ -41,3 +41,35 @@ JOIN suppliers ON suppliers.manufacturer_id = manufacturer.id
 JOIN goods ON goods.supplier_id = suppliers.id
 JOIN prices ON prices.goods_id = goods.id
 WHERE manufacturer.location = 'Moscow'
+
+--Задание 5
+SELECT author.name_author, book.title, COUNT(buy_book.amount) AS 'Количество'
+FROM book
+INNER JOIN author ON book.author_id = author.author_id
+LEFT JOIN buy_book ON book.book_id = buy_book.book_id
+GROUP BY author.name_author, book.title
+ORDER BY author.name_author, book.title
+
+--Задание 6
+SELECT name_genre, SUM(buy_book.amount) AS Количество
+FROM genre
+INNER JOIN book USING(genre_id)
+INNER JOIN buy_book USING(book_id)
+GROUP BY name_genre
+HAVING SUM(buy_book.amount) = ((SELECT MAX(sum_amount) AS max_sum_amount 
+            FROM (SELECT SUM(buy_book.amount) AS sum_amount 
+            FROM buy_book 
+            INNER JOIN book ON buy_book.book_id=book.book_id
+            INNER JOIN genre ON book.genre_id=genre.genre_id
+            GROUP BY genre.genre_id) g))
+
+--Задание 7
+SELECT buy_step.buy_id, (DATEDIFF(date_step_end, date_step_beg)) AS Количество_дней,
+               IF(((DATEDIFF(date_step_end, date_step_beg)) - days_delivery) <0, 0, ((DATEDIFF(date_step_end, date_step_beg)) - days_delivery)) AS Опоздание
+FROM buy_step
+INNER JOIN step ON buy_step.step_id = step.step_id
+INNER JOIN buy ON buy_step.buy_id = buy.buy_id
+INNER JOIN client ON buy.client_id = client.client_id
+INNER JOIN city ON client.city_id = city.city_id
+WHERE date_step_end IS NOT NULL AND name_step = 'Транспортировка'
+ORDER BY buy_step.buy_id
